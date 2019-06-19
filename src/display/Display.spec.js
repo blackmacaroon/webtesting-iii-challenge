b56912e6ba24d0066328bfb6d8f577bc1c886f9b
Kayla <6lackmacaroon@gmail.com>
2019-06-19 13:50:14 -0600

    essentials imported to tests.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,4 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
