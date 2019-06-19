86b7f7de02466e0b57d9eec3e1e5a9bc635ffb0e
Kayla <6lackmacaroon@gmail.com>
2019-06-19 14:09:56 -0600

    passing tests in dashboard to rendering page and child components

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,3 +2,24 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
+
+import Dashboard from './Dashboard.js';
+import { exportAllDeclaration } from '@babel/types';
+import Controls from '../controls/Controls.js';
+
+describe('<Dashboard />', () => {
+      it('should render the dashboard component without error', () => {
+            render(<Dashboard />);
+      })
+
+      it('should contain both Display and Controls child components', () => {
+            render(<Dashboard />);
+            // had Controls as argument, sent the whole damn page. I just want the word to show up.
+            expect(document.querySelector(".controls")).toBeTruthy();
+            expect(document.querySelector(".display")).toBeTruthy();
+            
+            // const { queryByText } = render(<Dashboard />);
+            // expect(queryByText(/display/i)).toBeTruthy();
+            // expect(queryByText(/controls/i)).toBeTruthy();
+      })
+});
