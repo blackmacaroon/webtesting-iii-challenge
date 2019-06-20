03140e842e49521079448b885b04346a2555b3e3
Kayla <6lackmacaroon@gmail.com>
2019-06-19 19:49:28 -0600

    toggle opene/closed passing tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import "jest-dom/extend-expect"; 
 import renderer from 'react-test-renderer';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 
-import Controls from './Controls';
+import Controls from './Controls.js';
+import Dashboard from '../dashboard/Dashboard.js'
 
 describe('<Controls />', () => {
       it('should render the controls component without errors', () => {
+            //renders the controls component
             render(<Controls />)
       })
+
+      it('should render initial state, unlocked and open', () => {
+            const { getByText } = render(<Dashboard />);
+            //renders the dashboard component and finds gate both unlocked and opened, with an option to close
+            getByText(/unlocked/i)
+            getByText(/open/i)
+            getByText(/close gate/i)
+      })
+
+      describe('open/closed toggle', () => {
+            it('should close, from initial open state, when close gate button is clicked', async () => {
+                  //make sure the gate is opened and button displays close gate option 
+                  const { getByText } = render(<Dashboard />)
+                  const toggle = getByText(/close gate/i)
+                  // waits til button it clicked, gate closes
+                  await fireEvent.click(toggle)
+                  // button will change to say 'open gate'
+                  getByText(/open gate/i)
+            })
+
+            it('should close, then open, when open gate button is clicked', async () => {
+                  const { getByText } = render(<Dashboard />)
+                  const toggle = getByText(/close gate/i)
+                  //closes gate 
+                  await fireEvent.click(toggle)
+                  // button changes to open
+                  getByText(/open gate/i)
+                  //opens gate
+                  await fireEvent.click(toggle)
+                  //button changes to close
+                  getByText(/close gate/i)
+            })
+      })
+
 })
